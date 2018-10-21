5805254719b9e8abf1ad7e68719f8dc49cc2d409
Meghan Campbell <mlc03e@gmail.com>
2018-10-21 18:33:50 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,7 +5,7 @@ end
 
 
 def shipping_manifest
-  shipping_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil painting" => 3 }
+  the_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil painting" => 3 }
 end
 
 def retrieval
