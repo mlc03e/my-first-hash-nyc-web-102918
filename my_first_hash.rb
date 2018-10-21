92d6298c72bcd54acb6e06d68e60cf168f2a5c02
Meghan Campbell <mlc03e@gmail.com>
2018-10-21 18:31:00 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,7 +5,7 @@ end
 
 
 def shipping_manifest
-  the_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil painting" => 3 }
+  shipping_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil painting" => 3 }
 end
 
 def retrieval
